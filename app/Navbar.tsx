5f3de56a6ecb063ca82e0843d5408b7dc1df24e5
prateekvarma <prtk.varma@gmail.com>
2023-10-23 16:07:22 -0600

    Created the navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+export default function Navbar() {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800 trasition-colors"
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
