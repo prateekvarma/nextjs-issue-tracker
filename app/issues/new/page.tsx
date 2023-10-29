db297707b8a1874a1f0a531ae09a061372eced5a
prateekvarma <prtk.varma@gmail.com>
2023-10-29 11:06:46 -0600

    Displaying generic error on form validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface IssueForm {
@@ -14,22 +15,31 @@ export default function NewIssuePage() {
   const router = useRouter();
   const { register, handleSubmit } = useForm<IssueForm>();
 
+  const [error, setError] = useState("");
+
   const submitHandler = async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
-      console.log(error);
+      setError("Unexpected Error");
     }
   };
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(submitHandler)}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <TextArea placeholder="Description" {...register("description")} />
-      <Button type="submit">Submit new Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(submitHandler)}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <TextArea placeholder="Description" {...register("description")} />
+        <Button type="submit">Submit new Issue</Button>
+      </form>
+    </div>
   );
 }
