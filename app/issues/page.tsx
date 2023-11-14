072edc822e06f4e42b36af85b5bab9949311b8d6
prateekvarma <prtk.varma@gmail.com>
2023-11-14 11:50:34 -0600

    Implemented skeleton loading on issues page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,9 +2,11 @@ import prisma from "@/prisma/client";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import delay from "delay";
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
+  await delay(2000);
 
   return (
     <div>
