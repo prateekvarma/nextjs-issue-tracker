ae849152ed8214f84a1d03f320c7d9388fa863a6
prateekvarma <prtk.varma@gmail.com>
2023-11-24 16:11:20 -0600

    Created separate Link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,9 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
+import Link from "../components/Link";
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
@@ -25,7 +25,9 @@ export default async function IssuesPage() {
             return (
               <Table.Row key={issue.id}>
                 <Table.Cell>
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                  <Link href={`/issues/${issue.id}`}>
+                    {issue.title}
+                  </Link>
                 </Table.Cell>
                 <Table.Cell>
                   <IssueStatusBadge status={issue.status} />
